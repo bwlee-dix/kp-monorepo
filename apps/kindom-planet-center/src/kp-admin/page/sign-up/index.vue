8b0e43271e313da88712e99c61aa5ae97bce017c
daeun lee <dix.delee@gmail.com>
2025-08-21 15:58:02 +0900

    feat(sign-up): add sign-up page & routing from /auth

--- apps/kindom-planet-center/src/kp-admin/page/sign-up/index.vue
+++ apps/kindom-planet-center/src/kp-admin/page/sign-up/index.vue
@@ -1,123 +1,120 @@
 <script setup lang="ts">
-import { useDarkmode } from '/@dds/stores/darkmode'
-import { useUserSession } from '/@dds/stores/userSession'
 import { useNotyf } from '/@dds/composable/useNotyf'
+
+import { useDarkmode } from '/@dds/stores/darkmode'
 import sleep from '/@dds/utils/sleep'
 
-const isLoading = ref(false)
 const darkmode = useDarkmode()
 const router = useRouter()
 const notyf = useNotyf()
-const userSession = useUserSession()
+const isLoading = ref(false)
 
-useHead({
-  title: 'Sign in - Admin - Kingdom Planet',
-})
-
-const handleLogin = async () => {
+const handleSignup = async () => {
   if (!isLoading.value) {
     isLoading.value = true
-
-    await sleep(1000)
-    userSession.setToken('logged-in')
+    sleep(2000)
 
     notyf.dismissAll()
-    notyf.success('Welcome back, Erik Kovalsky')
-
-    router.replace('/app-list')
-
+    notyf.success('Welcome, Erik Kovalsky')
+    router.push('/auth')
     isLoading.value = false
   }
 }
+
+useHead({
+  title: 'Sign UP - Admin - Kingdom Planet',
+})
 </script>
 
 <template>
-  <div class="auth-wrapper">
-    <div class="auth-wrapper-inner is-single">
-      <!--Fake navigation-->
-      <div class="auth-nav">
-        <div class="left" />
-        <div class="center">
-          <RouterLink to="/" class="header-item">
-            <AnimatedLogo width="38px" height="38px" />
-          </RouterLink>
-        </div>
-        <div class="right">
-          <label
-            class="ml-auto dark-mode"
-            tabindex="0"
-            role="button"
-            @keydown.space.prevent="
-              (e: KeyboardEvent) => (e.target as HTMLLabelElement).click()
-            "
-          >
-            <input
-              data-cy="dark-mode-toggle"
-              type="checkbox"
-              :checked="!darkmode.isDark"
-              @change="darkmode.onChange"
-            />
-            <span />
-          </label>
-        </div>
+  <div class="auth-wrapper-inner is-single">
+    <!--Fake navigation-->
+    <div class="auth-nav">
+      <div class="left" />
+      <div class="center">
+        <RouterLink to="/" class="header-item">
+          <AnimatedLogo width="38px" height="38px" />
+        </RouterLink>
       </div>
+      <div class="right">
+        <label
+          class="dark-mode ml-auto"
+          tabindex="0"
+          role="button"
+          @keydown.space.prevent="(e) => (e.target as HTMLLabelElement).click()"
+        >
+          <input
+            data-cy="dark-mode-toggle"
+            type="checkbox"
+            :checked="!darkmode.isDark"
+            @change="darkmode.onChange"
+          />
+          <span />
+        </label>
+      </div>
+    </div>
 
-      <!--Single Centered Form-->
-      <div class="single-form-wrap">
-        <div class="inner-wrap">
-          <!--Form Title-->
-          <div class="auth-head">
-            <h2>Welcome Back.</h2>
-            <p>Please sign in to your account</p>
-            <RouterLink to="/sign-up"> I do not have an account yet </RouterLink>
-          </div>
+    <!--Single Centered Form-->
+    <div class="single-form-wrap">
+      <div class="inner-wrap">
+        <!--Form Title-->
+        <div class="auth-head">
+          <h2>Join Us Now.</h2>
+          <p>Start by creating your account</p>
+          <RouterLink to="/auth/"> I already have an account </RouterLink>
+        </div>
 
-          <!--Form-->
-          <div class="form-card">
-            <form method="post" novalidate @submit.prevent="handleLogin">
-              <div class="login-form">
-                <VField>
-                  <VControl icon="feather:user">
-                    <VInput type="text" placeholder="Username" autocomplete="username" />
-                  </VControl>
-                </VField>
-                <VField>
-                  <VControl icon="feather:lock">
-                    <VInput
-                      type="password"
-                      placeholder="Password"
-                      autocomplete="current-password"
-                    />
-                  </VControl>
-                </VField>
+        <!--Form-->
+        <div class="form-card">
+          <form method="post" novalidate @submit.prevent="handleSignup">
+            <div class="login-form">
+              <!-- Input -->
+              <VField>
+                <VControl icon="feather:user">
+                  <VInput type="text" placeholder="Name" autocomplete="name" />
+                </VControl>
+              </VField>
+              <!-- Input -->
+              <VField>
+                <VControl icon="feather:mail">
+                  <VInput type="text" placeholder="Email Address" autocomplete="email" />
+                </VControl>
+              </VField>
+              <!-- Input -->
+              <VField>
+                <VControl icon="feather:lock">
+                  <VInput
+                    type="password"
+                    placeholder="Password"
+                    autocomplete="new-password"
+                  />
+                </VControl>
+              </VField>
+              <!-- Input -->
+              <VField>
+                <VControl icon="feather:lock">
+                  <VInput type="password" placeholder="Repeat Password" />
+                </VControl>
+              </VField>
 
-                <!-- Switch -->
-                <VField>
-                  <VControl class="setting-item">
-                    <VCheckbox label="Remember me" color="primary" paddingless />
-                  </VControl>
-                </VField>
-
-                <!-- Submit -->
-                <div class="login">
-                  <VButton
-                    :loading="isLoading"
-                    type="submit"
+              <VField>
+                <VControl class="setting-item">
+                  <VCheckbox
+                    label="Receive promotional offers"
                     color="primary"
-                    bold
-                    fullwidth
-                    raised
-                  >
-                    Sign In
-                  </VButton>
-                </div>
-              </div>
-            </form>
-          </div>
+                    paddingless
+                  />
+                </VControl>
+              </VField>
 
-          <div class="forgot-link has-text-centered">
-            <a>Forgot Password?</a>
-          </div>
+              <!-- Submit -->
+              <div class="login">
+                <VButton color="primary" type="submit" bold fullwidth raised>
+                  Sign Up
+                </VButton>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
